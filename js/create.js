45dcb01769842cfc86af21ab2f24c686a4fe4b9f
tsunabit <tsuniki.miniki@gmail.com>
2018-03-31 21:12:58 +0900

    軽微な修正

--- js/create.js
+++ js/create.js
@@ -5,8 +5,8 @@
 $(function() {
 	$('input[name=name]').bind('blur' , function() {
 		if($(this).val() == ''){
-			// $(this).addClass('error').parent().append('<p class="error">※この項目は必ず入力してください</p>');
-			$(this).addClass('error').append('<p class="error">※この項目は必ず入力してください</p>');
+			$(this).addClass('error').parent().append('<p class="error">※この項目は必ず入力してください</p>');
+			// $(this).addClass('error').append('<p class="error">※この項目は必ず入力してください</p>');
 		}
 	})
 	.bind('focus' , function() {
